5cd7c3679a906c7cdf5f4eeeba1bccc52b37a5e8
Dunya <dunya@RenoRuns-MacBook-Pro-3.local>
2020-01-05 14:13:45 +0300

    add dotenv variables

---
+++ create_tables.py
@@ -0,0 +1,35 @@
+from dotenv import load_dotenv, find_dotenv
+import os
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+HOST = os.environ['HOST']
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_PORT = os.environ['DB_PORT']
+
+def drop_tables(cur, conn):
+    for query in drop_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def create_tables(cur, conn):
+    for query in create_table_queries:
+        cur.execute(query)
+        conn.commit()
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
+
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(HOST, DB_NAME, DB_USER, DB_PASSWORD, DB_PORT)
+    cur = conn.cursor()
+
+    drop_tables(cur, conn)
+    create_tables(cur, conn)
+
+    conn.close()
+
+if __name__ == "__main__":
+    main()
